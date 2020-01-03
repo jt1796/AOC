6ecfabeba05765a73cdd92cca017a62603b13166
John Tompkins <tompkins.jt@gmail.com>
2020-01-03 17:21:16 -0500

    Redo pt1 using just math. May be helpful for pt2

--- 22.groovy
+++ 22.groovy
@@ -1,38 +1,21 @@
-deck = (0..(10007 - 1)) as LinkedList
+decksize = 10007 as BigInteger
+position = 2019
+
+// 3377
+
 def newstack() {
-    println "newstack"
-    newdeck = [] as LinkedList
-    for (card in deck) {
-        newdeck.addFirst(card)
-    }
-    deck = newdeck
+    position = decksize - position - 1 
 }
 
 def deal(int n) {
-    println "deal $n"
-    int pos = 0;
-    newdeck = [] as LinkedList
-    deck.size().times { newdeck.addFirst(-1) }
-    while (!deck.isEmpty()) {
-        newdeck.set(pos, deck.removeFirst())
-        pos = (pos + n) % newdeck.size()
-    }
-    deck = newdeck
+    position = ((n * position) % decksize)
 }
 
 def cut(int n) {
-    println "cut $n"
-    def addr = n > 0 ? deck.&addLast   : deck.&addFirst
-    def rmr =  n > 0 ? deck.&removeFirst  : deck.&removeLast
-    Math.abs(n).times {
-        addr(rmr())
-    }
+    position = (position - n + decksize) % decksize
 }
 
-println deck
-
 moves = new File('22.txt').readLines().collect{ it.split(' ') }
-
 for (move in moves) {
     if (move == ['deal', 'into', 'new', 'stack']) {
         newstack()
@@ -45,5 +28,4 @@ for (move in moves) {
     }
 }
 
-println deck
-println deck.indexOf(2019)
+println position
