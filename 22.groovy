8d00462d4080298b74465eade16bbfbf2740c449
John Tompkins <tompkins.jt@gmail.com>
2020-01-04 14:59:02 -0500

    Rename card operations into their true names; less confusing

--- 22.groovy
+++ 22.groovy
@@ -2,22 +2,22 @@ decksize = 119315717514047 as BigInteger
 initialposition = 2020 as BigInteger
 position = initialposition
 
-def deal(BigInteger n) {
+def multiply(BigInteger n) {
     position = ((n.modInverse(decksize) * position) % decksize)
 }
 
-def cut(BigInteger n) {
+def add(BigInteger n) {
     position = (position + n + decksize) % decksize
 }
 
 moves = new File('22.txt').readLines().collect{ it.split(' ') }
 def shuffle() {
     for (move in moves.reverse()) {
-        if (move.size() > 2 && move[0..2] == ['deal', 'with', 'increment']) {
-            deal(move[3].toBigInteger())
+        if (move[0] == 'multiply') {
+            multiply(move[1].toBigInteger())
         }
-        if (move[0] == 'cut') {
-            cut(move[1].toBigInteger())
+        if (move[0] == 'add') {
+            add(move[1].toBigInteger())
         }
     }
 }
