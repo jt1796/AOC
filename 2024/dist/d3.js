cbac3c3bf2321793f9b5f5f7e5812bd6755ce6d1
John Tompkins <tompkins.jt@gmail.com>
2024-12-16 21:52:19 -0500

    Remove dist

--- 2024/dist/d3.js
+++
@@ -1,5 +0,0 @@
-import { exhaust } from "./common.js";
-const input = "d3.txt".readString().replace(/don't\(\).*?do\(\)/sg, '');
-const mul = /mul\((\d+),(\d+)\)/g;
-const res = exhaust(() => mul.exec(input)).map(m => +m[1] * +m[2]).sum();
-console.log({ res });
--- 2024/dist/d4.js
+++
@@ -1,5 +0,0 @@
-import "./common.js";
-const grid = "d4.txt".readStringGrid();
-const dirsA = [[-1, -1], [0, 0], [1, 1]];
-const dirsB = [[-1, 1], [0, 0], [1, -1]];
-grid.flatMap((row, x) => row.flatMap((i, y) => [dirsA, dirsB].every(dir => dir.map(([dx, dy]) => { var _a; return (_a = grid[x + dx]) === null || _a === void 0 ? void 0 : _a[y + dy]; }).join('') in { 'MAS': 1, 'SAM': 1 }))).sum().print();
